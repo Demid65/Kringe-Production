7495359a5e6c6f2e1eaf68cbd3a1f401839daf08
Mierley <88342799+Mierley@users.noreply.github.com>
2023-06-25 23:36:54 +0300

    Added new models
    Added models files, category, course

--- api/base/admin.py
+++ api/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from base import models
+
+admin.site.register(models.Course)
+admin.site.register(models.File)
+admin.site.register(models.Category)
