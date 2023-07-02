bf322e232afe6c70331053c24d4d056f37f31d41
Tufra <60471036+Tufra@users.noreply.github.com>
2023-07-02 14:04:42 +0300

    frontend fixes

--- frontend/utils/routes.ts
+++ frontend/utils/routes.ts
@@ -1,6 +1,6 @@
 export const routesMap = {
     sidebarData: '/api/mocks/',
-    mainData: '/api/mocks/',
+    mainData: '/api/courses/main-page',
     courseData: '/api/mocks/',
     courseDiscussion: '/api/mocks',
     getFile: '/api/getfile/',
